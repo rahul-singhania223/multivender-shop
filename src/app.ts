3bb733ce524e10993d3e9cf191cef0fd21435703
RA.one <rahulsinghania406@gmail.com>
2023-12-05 21:51:04 +0530

    added app configurations

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import ApiError from "./utils/ApiError";
 
 const app = express();
+
+// app config
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
+
+// ROUTES
+
+// handle unknown routes
+app.get("*", (req, res, next) => {
+  next(new ApiError("Unknown route", 404));
+});
 
 export { app };
