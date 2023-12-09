ce93098faef219efe18e1f8b983f1b618d171eb4
RA.one <rahulsinghania406@gmail.com>
2023-12-09 23:37:39 +0530

    added redis url in env

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,7 +1,5 @@
 import Redis from "ioredis";
 
-const redis = new Redis(
-  "redis://default:845167ecc98b43c29fb188fb9e832600@us1-harmless-oriole-41192.upstash.io:41192"
-);
+const redis = new Redis(process.env.REDIS_URL as string);
 
 export { redis };
